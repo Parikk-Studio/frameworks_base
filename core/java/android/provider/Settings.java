18286329089e6266fc21100106ed81f5c4a131b1
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-11-10 01:20:15 +0530

    SystemUI: Make advanced reboot optional
    Allow toggling it, and disable it by default.
    
    Change-Id: If10fbf5dc591858255e399108758a9e5b4ca9cfb
    Signed-off-by: rk134 <rahul@aospa.co>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5840,7 +5840,7 @@ public final class Settings {
          *     <li> 1 = On
          * </ul>
          * @hide
-         */
+        */
         public static final String CAMERA_FLASH_NOTIFICATION = "camera_flash_notification";
 
         /**
@@ -6046,6 +6046,12 @@ public final class Settings {
          * @hide
          */
         public static final String VIBRATE_ON_DISCONNECT = "vibrate_on_disconnect";
+
+        /**
+         * Whether to show advanced reboot options in power menu
+         * @hide
+         */
+        public static final String ADVANCED_REBOOT = "advanced_reboot";
 
         /**
          * Keys we no longer back up under the current schema, but want to continue to
